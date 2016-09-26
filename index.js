1f842a655ddf8202eca6524b6fc9306fe84384e1
Joscha Feth <joscha@feth.com>
2016-09-27 00:11:28 +1000

    refactor: Remove console.log

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const loaderUtils = require('loader-utils');
 module.exports = function(source) {
   this.cacheable && this.cacheable();
   const query = loaderUtils.parseQuery(this.query);
-  console.log('query', query);
   const camelCaseKeys = !!(query.camelCase || query.camelcase);
 
   const vars = lessToJs(source);
-  console.log('source', source);
   const keys = Object.keys(vars);
   if (!keys.length) {
     this.emitWarning('Could not find any extractable less variables!');
@@ -20,15 +18,14 @@ module.exports = function(source) {
   const transformKey = (key) => {
       let ret = key.replace(/^@/, '');
       if (camelCaseKeys) {
-          console.log('XXX');
-          ret = camelcase(ret);
+        ret = camelcase(ret);
       }
       return ret;
   }
 
   const cleanedVars = keys.reduce((prev, key) => {
-      prev[transformKey(key)] = vars[key];
-      return prev;
+    prev[transformKey(key)] = vars[key];
+    return prev;
   }, {});
 
   return `module.exports = ${JSON.stringify(cleanedVars, true, 2)};\n`;
